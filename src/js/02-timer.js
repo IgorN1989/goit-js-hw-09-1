39984ee517ede1c7434e9c3051bef3fd278d3463
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-07 19:42:30 +0200

    update

---
+++ src/js/02-timer.js
@@ -0,0 +1,13 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startButton: document.querySelector('button'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+flatpickr(refs.input, options);
