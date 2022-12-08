cbfab73e17b26a129f992304afe42669c2fabbc8
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-08 02:50:07 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,24 +4,41 @@ require('flatpickr/dist/themes/material_green.css');
 
 const refs = {
   input: document.querySelector('#datetime-picker'),
-  startButton: document.querySelector('button'),
+  startBtn: document.querySelector('button'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
 
-const startDate = new Date();
+refs.startBtn.addEventListener('click', onStartBtnClick);
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+function onStartBtnClick(evt) {
+  console.log(evt);
+}
 
-flatpickr(refs.input, options);
-console.log(refs.input);
+const futureDate = '2030-03-16T14:25:00';
+
+class Timer {
+  constructor(rootSelector, targetDate) {
+    this.rootSelector = rootSelector;
+    this.targetDate = targetDate;
+  }
+}
+
+const timer = new Timer('.timer', futureDate);
+
+console.log(timer);
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// flatpickr(refs.input, options);
+// console.log(refs.input);
